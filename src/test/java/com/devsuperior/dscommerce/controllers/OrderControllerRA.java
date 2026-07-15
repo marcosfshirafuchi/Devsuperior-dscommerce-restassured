1150175ccf6f04908da05315374f7e6d47ae46b0
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2026-07-15 19:36:03 -0300

    Outros cenários da consulta de pedido por id

--- src/test/java/com/devsuperior/dscommerce/controllers/OrderControllerRA.java
+++ src/test/java/com/devsuperior/dscommerce/controllers/OrderControllerRA.java
@@ -71,17 +71,95 @@ public class OrderControllerRA {
     }
 
     //2.	Busca de pedido por id retorna pedido existente quando logado como cliente e o pedido pertence ao usuário
-
+    @Test
+    public void findByIdShouldReturnOrderWhenIdExistsAndClientLogged(){
+        given()
+           //Definindo o cabeçalho da requisição
+           //Tipo da informação
+           .header("Content-type", "application/json")
+           .header("Authorization", "Bearer " + clientToken)
+           .accept(ContentType.JSON)
+        .when()
+           //Está passando o endpoint para testar
+           .get("/orders/{id}", existingOrderId)
+        .then()
+           //Verificando a resposta da requisição
+           .statusCode(200)
+           .body("id", is(1))
+           .body("moment", equalTo("2022-07-25T13:00:00Z"))
+           .body("status", equalTo("PAID"))
+           .body("client.name", equalTo("Maria Brown"))
+           .body("payment.moment", equalTo("2022-07-25T15:00:00Z"))
+           .body("items.name", hasItems("The Lord of the Rings", "Macbook Pro"))
+           .body("total", is(1431.0F));
+    }
 
     //3.	Busca de pedido por id retorna 403 quando pedido não pertence ao usuário
-
+    @Test
+    public void findByIdShouldReturnForbiddenWhenIdExistsAndClientLoggedAndOrderDoesNotBelongUser(){
+        Long otherOrderId = 2L;
+        given()
+           //Definindo o cabeçalho da requisição
+           //Tipo da informação
+           .header("Content-type", "application/json")
+           .header("Authorization", "Bearer " + clientToken)
+           .accept(ContentType.JSON)
+        .when()
+           //Está passando o endpoint para testar
+           .get("/orders/{id}", otherOrderId)
+        .then()
+           //Verificando a resposta da requisição
+           .statusCode(403);
+    }
 
     //4.	Busca de pedido por id retorna 404 para pedido inexistente quando logado como admin
-
+    @Test
+    public void findByIdShouldReturnNotFoundWhenIdDoesNotExistAndAdminLogged(){
+        given()
+           //Definindo o cabeçalho da requisição
+           //Tipo da informação
+           .header("Content-type", "application/json")
+           .header("Authorization", "Bearer " + adminToken)
+           .accept(ContentType.JSON)
+        .when()
+           //Está passando o endpoint para testar
+           .get("/orders/{id}", nonExistingOrderId)
+        .then()
+           //Verificando a resposta da requisição
+           .statusCode(404);
+    }
 
     //5.	Busca de pedido por id retorna 404 para pedido inexistente quando logado como cliente
-
+    @Test
+    public void findByIdShouldReturnNotFoundWhenIdDoesNotExistAndClientLogged(){
+        given()
+           //Definindo o cabeçalho da requisição
+           //Tipo da informação
+           .header("Content-type", "application/json")
+           .header("Authorization", "Bearer " + clientToken)
+           .accept(ContentType.JSON)
+        .when()
+           //Está passando o endpoint para testar
+           .get("/orders/{id}", nonExistingOrderId)
+        .then()
+           //Verificando a resposta da requisição
+           .statusCode(404);
+    }
 
     //6.	Busca de pedido por id retorna 401 quando não logado como admin ou cliente
-
+    @Test
+    public void findByIdShouldReturnUnauthorizedWhenIdExistsAndInvalidToken(){
+        given()
+           //Definindo o cabeçalho da requisição
+           //Tipo da informação
+           .header("Content-type", "application/json")
+           .header("Authorization", "Bearer " + invalidToken)
+           .accept(ContentType.JSON)
+        .when()
+           //Está passando o endpoint para testar
+           .get("/orders/{id}", existingOrderId)
+        .then()
+           //Verificando a resposta da requisição
+           .statusCode(401);
+    }
 }
